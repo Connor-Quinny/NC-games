2ddb6b534980806246344a8daa1913eb0b4da90a
Connor-Quinny <cmquinn98@gmail.com>
2022-09-30 14:48:58 +0100

    Comments added and errors handled

--- src/Components/Comments.jsx
+++ src/Components/Comments.jsx
@@ -18,6 +18,9 @@ const Comments = () => {
         })
     },[review_id])
 
+    if (error === true) {
+        return <img className="error-img" src="https://i.stack.imgur.com/6M513.png" alt="404 error"></img>
+    }
     
 
     return (
